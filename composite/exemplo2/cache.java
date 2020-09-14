bbbbd93731c6197f1323a586f36c5a808b72ce36
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-09-14 19:59:59 -0300

    Create cache.java

---
+++ composite/exemplo2/cache.java
@@ -0,0 +1,12 @@
+public class Cache implements Component{
+int memoria;
+
+public Cache (int vel){
+memoria = vel;
+}
+public   void adiciona(Componente cm){
+}
+public   void executa(){
+System.out.println("cache com "+memoria +" MB");
+}
+}
