ec3620ebf9469c29219be39169847ecce74c5b56
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-09-14 12:52:37 -0300

    Create CamisetaComponent.java

---
+++ composite/CamisetaComponent.java
@@ -0,0 +1,29 @@
+public abstract class CamisetaComponent {
+
+
+ String marcaDaCamiseta;
+
+
+ public void printNomeDaCamiseta() {
+  System.out.println(this.marcaDaCamiseta);
+ }
+
+
+ public String getNomeDaCamiseta() {
+  return this.marcaDaCamiseta;
+ }
+
+
+ public void adicionar(CamisetaComponent novaCamiseta) throws Exception {
+  throw new Exception("erro");
+ }
+
+
+ public void remover(String marcaDaCamiseta) throws Exception {
+  throw new Exception("Erro");
+
+
+ public CamisetaComponent getCamiseta(String marcaDacamiseta) throws Exception {
+  throw new Exception("Erro);
+ }
+}
