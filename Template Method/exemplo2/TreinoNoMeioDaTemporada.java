39713b607bb57df6e488c5a818ee8364f17f4be5
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-09-16 13:02:11 -0300

    Create TreinoNoMeioDaTemporada.java

---
+++ Template Method/exemplo2/TreinoNoMeioDaTemporada.java
@@ -0,0 +1,10 @@
+class TreinoNoMeioDaTemporada extends Treinos {
+
+	void preparoFisico() {
+		System.out.println("Preparo Fisico Intenso.");
+	}
+
+	void jogoTreino() {
+		System.out.println("Jogo Treino com Equipe Reserva.");
+	}
+}
