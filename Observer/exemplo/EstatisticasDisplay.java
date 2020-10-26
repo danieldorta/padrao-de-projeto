12ae6ae02adeccf2091f29d1cab619a5a38005f7
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-10-26 12:51:33 -0300

    Create EstatisticasDisplay.java

---
+++ Observer/exemplo/EstatisticasDisplay.java
@@ -0,0 +1,13 @@
+public class EstatisticasDisplay {
+
+    private float temperatura;
+    private float umidade;
+    private float pressao;
+
+    private void display(){
+
+        System.out.println( "Temperatura: " + temperatura );
+        System.out.println( "Umidade: " + umidade );
+        System.out.println( "Pressao: " + pressao );
+    }
+}
