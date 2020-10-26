3134d88ea4a36fd5eec09a54cdf2ec39d700ae7f
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-10-26 12:51:09 -0300

    Create EstacaoMeteorologica.java

---
+++ Observer/exemplo/EstacaoMeteorologica.java
@@ -0,0 +1,16 @@
+public class EstacaoMeteorologica {
+
+    private float temperatura;
+    private float umidade;
+    private float pressao;
+
+    public void setMedicoes(
+        float temperatura,
+        float umidade,
+        float pressao ){
+
+        this.temperatura = temperatura;
+        this.umidade = umidade;
+        this.pressao = pressao;
+    }
+}
