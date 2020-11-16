812d130642d0cf1c0a891975f93be19559826f5c
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-11-16 15:00:23 -0300

    Create TigreToy.java

---
+++ Strategy/exemplo/TigreToy.java
@@ -0,0 +1,14 @@
+public class TigreToy extends Felino {
+
+    @Override
+    public void display(){
+
+        System.out.println( "Tigre de borracha na banheira" );
+    }
+
+    @Override
+    public void rugir(){
+
+        System.out.println( "Uen! Uen!" );
+    }
+}
