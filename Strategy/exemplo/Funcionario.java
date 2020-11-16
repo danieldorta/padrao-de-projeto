37ce1716fe18869ba18e0aad6981f3c65658170b
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-11-16 17:27:36 -0300

    Create Funcionario.java

---
+++ Strategy/exemplo/Funcionario.java
@@ -0,0 +1,14 @@
+ /**
+     * Dados do funcionario.
+     */
+    public class Funcionario {
+        private long id;
+        /**
+         * Definimos no corpo qual o tipo de implementação nossa
+         * variável cargo deverá receber (novos tipos podem surgir
+         * sem que haja muitos impactos), ex: cargo = Cargo.VENDEDOR;.
+         */
+        private Strategy cargo;
+        // Construtor, getters e setters
+        . . .
+    }
