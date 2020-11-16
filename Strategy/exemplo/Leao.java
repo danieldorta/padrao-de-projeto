b7b1994a1e41bcddfa571c219a7b0a45f496d951
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-11-16 14:59:31 -0300

    Create Leao.java

---
+++ Strategy/exemplo/Leao.java
@@ -0,0 +1,14 @@
+public class Leao extends Felino {
+
+    @Override
+    public void display(){
+
+        System.out.println( "Leão a vista" );
+    }
+
+    @Override
+    public void rugir(){
+
+        System.out.println( "Urghhhhh!" );
+    }
+}
