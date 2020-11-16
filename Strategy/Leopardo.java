bf09a2831ffe364fa921f8d8183260e1f5895ef8
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-11-16 14:59:52 -0300

    Create Leopardo.java

---
+++ Strategy/Leopardo.java
@@ -0,0 +1,14 @@
+public class Leopardo extends Felino {
+
+    @Override
+    public void display(){
+
+        System.out.println( "Leopardo a vista" );
+    }
+
+    @Override
+    public void rugir(){
+
+        System.out.println( "Arghh!" );
+    }
+}
