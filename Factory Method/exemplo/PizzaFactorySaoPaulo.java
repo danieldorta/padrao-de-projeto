130ed77f9437976743d3ea8ca567e7edc37ae8ce
danieldorta <71103252+danieldorta@users.noreply.github.com>
2020-11-26 19:03:56 -0300

    Create PizzaFactorySaoPaulo.java

---
+++ Factory Method/exemplo/PizzaFactorySaoPaulo.java
@@ -0,0 +1,19 @@
+public class PizzaFactorySaoPaulo extends PizzaFactory {
+
+    @Override
+    public void criarPizza( String tipo ){
+
+        if( tipo.equals( "queijo" ) ){
+
+            pizza = new SPPizzaQuatroQueijos();
+        }
+        else if( tipo.equals( "portuguesa" ) ){
+
+            pizza = new SPPizzaPortuguesa();
+        }
+        else if( tipo.equals( "calabresa" ) ){
+
+            pizza = new SPPizzaCalabresa();
+        }
+    }
+}
